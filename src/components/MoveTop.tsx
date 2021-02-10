bea89af928ba2f4452f4cf369071ed795b4845a0
SeoJun Park <jinchul112@gmail.com>
2021-02-10 20:18:47 +0100

    update

--- src/components/MoveTop.tsx
+++ src/components/MoveTop.tsx
@@ -12,6 +12,7 @@ const S = {
     Button: styled.button`
     width: 100%;
     padding: 12px;
+    background-color:#ffffff;
     border:1px solid ${props => props.theme.palette.primary};
     border-radius:1rem;
     color:${props => props.theme.palette.primary};
@@ -31,7 +32,7 @@ const Arrow = () => {
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            fill="white"
+            fill="#0063FF"
         >
             <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z" />
         </svg>
